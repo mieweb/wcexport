f7b69c8d5e773405576dfbf33020521f0135dad7
David Cornewell <david.cornewell@gmail.com>
2025-05-09 08:53:12 -0400

    * Added log text box to see what has happened ** This will show what is logged to the wcexport.log file. * Made the URL text wider * Added options to specify URL and username on commandline so re-running is easier ** ./wcexport.py [URL] [username] * If the user's login session expires, the next request will attempt to log in and resume

--- wcexport.py
+++ wcexport.py
@@ -3,10 +3,24 @@ import tk
 import tkinter
 
 import common
+import argparse
 
 def main():
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description="WebChart Export Utility")
+    parser.add_argument("url", nargs="?", help="WebChart URL (optional)")
+    parser.add_argument("username", nargs="?", help="WebChart username (optional)")
+    args = parser.parse_args()
+
     win = tkinter.Tk()
     app = common.MainWin(win)
+
+    # Set URL and username if provided
+    if args.url:
+        app.url.insert(0, args.url)
+    if args.username:
+        app.username.insert(0, args.username)
+
     win.mainloop()
     try:
         win.destroy()
